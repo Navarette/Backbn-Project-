c3e719b622e7d2599ebd6370a8bbf309d02c1138
Navarette <navarette.arnold@iisgalvanimi.edu.it>
2024-05-17 11:02:25 +0000

    putno fare chiamata http da Angular al server

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+//import { HttpClientModule} from ''
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+  // constructor(private http: HttpClientModule){
+  //   this.http.get("TUO_URL/simple_json")
+  //   .subscribe(
+  //     (data) => this.data = data
+  //   )
+  // }
 }
